5e4a9a716b1193e905c82ec8c7fd81be07731e0c
dwivediprashant <dwivediprashant.0219@gmail.com>
2026-01-20 16:12:06 +0530

    Update : Added protected route sample

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { authSignupController } from "../controllers/authSignupController";
 import { authLoginController } from "../controllers/authLoginController";
-
+import { authenticateUser } from "../middleware/authenticateUser";
 const router = Router();
+router.get("/auth/me", authenticateUser, (req, res) => {
+  res.json({ userId: req.user?.id });
+});
 
 router.post("/auth/signup", authSignupController);
 router.post("/auth/login", authLoginController);
